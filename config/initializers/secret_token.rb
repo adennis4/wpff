abb5ef6738f4e0cc8e3ff39e1a97ebb8081f2a38
Hashrocket Workstation <adennis4@gmail.com>
2013-10-24 21:49:16 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wpff::Application.config.secret_key_base = '620537e27320a1dccc36f2b21002c133945ddab4bc103d948c337e9df1729139b6653e38ff97ecdeb07c1ab61394619862a1124702b225af688b87d7d6938c41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wpff::Application.config.session_store :cookie_store, key: '_wpff_session'
